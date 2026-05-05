b95e267dda80c5a0732584077c4c1e1c6134fd32
mathewdillian02 <mathewdillian02@gmail.com>
2026-05-05 17:58:42 -0400

    commit #1

---
+++ app.py
@@ -0,0 +1,13 @@
+#website app
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    print("Starting app")
+    app.run(debug=True)
